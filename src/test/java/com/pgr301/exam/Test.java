781ef3ae912b09f980eeef32339f430ac0dd755c
GeorgTollefsen <georgto@me.com>
2021-11-24 13:47:34 +0100

    added a test

--- src/test/java/com/pgr301/exam/Test.java
+++ src/test/java/com/pgr301/exam/Test.java
@@ -1,16 +1,13 @@
 package com.pgr301.exam;
 
 import com.pgr301.exam.model.Account;
-import org.junit.jupiter.api.function.Executable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
 
 public class Test {
 
-
     @org.junit.jupiter.api.Test
     public void create(){
-        int x = 1;
-        org.junit.jupiter.api.Assertions.assertTrue(x==1);
+        Account x = new Account();
+        Assertions.assertEquals("0", x.getBalance().toString());
     }
 }
