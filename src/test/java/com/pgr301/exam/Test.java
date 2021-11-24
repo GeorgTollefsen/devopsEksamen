df545c3a4c2fe65db142c23a8a1be96d887c3a4c
GeorgTollefsen <georgto@me.com>
2021-11-24 13:23:47 +0100

    added a test

---
+++ src/test/java/com/pgr301/exam/Test.java
@@ -0,0 +1,16 @@
+package com.pgr301.exam;
+
+import com.pgr301.exam.model.Account;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class Test {
+
+
+    @org.junit.jupiter.api.Test
+    public void create(){
+        int x = 1;
+        org.junit.jupiter.api.Assertions.assertTrue(x==1);
+    }
+}
